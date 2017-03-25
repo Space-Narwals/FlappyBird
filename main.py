7c6fa85afc4a9a8fd7ded914a3c4d1416971ab99
RenegadeEagle <v3rglas@gmail.com>
2017-03-25 15:33:28 -0500

    Fix scrolling background. Massive cleanup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pygame;
-import os;
-import random;
-
+import pygame
+import os
+import random
 
 version = "1.0.0 BETA"
 
@@ -9,13 +8,21 @@ clock = pygame.time.Clock()
 
 debug = True
 SCREEN_WIDTH = 144
-SCEREN_HEIGHT = 256;
-SCALE = 3;
+SCREEN_HEIGHT = 256
+SCALE = 3
 
-TICKS_TO_SPAWN = 120;
+#How many ticks should we wait before spawnning another pair of pipes?
+TICKS_TO_SPAWN = 120
+
+PIPE_HEIGHT = 159
+PIPE_WIDTH = 26
+
+BACKGROUND_WIDTH = 168
+BACKROUND_HEIGHT = 56
+#Represents the Y cordinate of where the scrolling background begins.
+BACKGROUND_Y_START = 200
 
 spriteimage = pygame.image.load('res/sprites.png')
-
 
 class Pipe(pygame.sprite.Sprite):
     def __init__(self, side, y):
@@ -23,76 +30,90 @@ class Pipe(pygame.sprite.Sprite):
         # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
 
-        pipeImage = pygame.Surface((26, 159));
+        pipeImage = pygame.Surface((PIPE_WIDTH, PIPE_HEIGHT))
         if side == "bottom":
-            pipeImage.blit(spriteimage, (0, 0), (84, 323, 26, 159))
+            pipeImage.blit(spriteimage, (0, 0), (84, 323, PIPE_WIDTH, PIPE_HEIGHT))
         else:
-            pipeImage.blit(spriteimage, (0, 0), (56, 323, 26, 159))
-        pipeImage = pygame.transform.scale(pipeImage, (26 * SCALE, 159 * SCALE));
+            pipeImage.blit(spriteimage, (0, 0), (56, 323, PIPE_WIDTH, PIPE_HEIGHT))
+        pipeImage = pygame.transform.scale(pipeImage, (PIPE_WIDTH * SCALE, PIPE_HEIGHT * SCALE))
 
-        self.image = pipeImage;
+        self.image = pipeImage
         self.rect = self.image.get_rect()
-        self.rect.x = 144 * SCALE + 50;
-        self.rect.y = y;
+        #Spawn the pipes 50 pixels to the right of the
+        self.rect.x = 144 * SCALE + 50
+        self.rect.y = y
 
     def update(self):
         self.rect.x = self.rect.x - 3
-        if(self.rect.x < 0 - (26 * SCALE)):
+        #If the sprite goes off the screen, destroy it.
+        if (self.rect.x < 0 - (PIPE_WIDTH * SCALE)):
             self.kill()
 
 class ScrollingBackground(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, startX):
         pygame.sprite.Sprite.__init__(self)
-        scrollingBackground = pygame.Surface((168, 56));
-        scrollingBackground.blit(spriteimage, (0, 0), (292, 0, 168, 56))
-        scrollingBackground = pygame.transform.scale(scrollingBackground, (168 * SCALE, 56 * SCALE));
-        self.image = scrollingBackground;
-        self.rect = self.image.get_rect();
-        self.rect.x = 0;
-        self.rect.y = 200 * SCALE;
-
+        scrollingBackground = pygame.Surface((BACKGROUND_WIDTH, BACKROUND_HEIGHT))
+        scrollingBackground.blit(spriteimage, (0, 0), (292, 0, BACKGROUND_WIDTH, BACKROUND_HEIGHT))
+        scrollingBackground = pygame.transform.scale(scrollingBackground, (BACKGROUND_WIDTH * SCALE, BACKROUND_HEIGHT * SCALE))
+        self.image = scrollingBackground
+        self.rect = self.image.get_rect()
+        self.rect.x = startX
+        self.rect.y = BACKGROUND_Y_START * SCALE
+    def update(self):
+        self.rect.x = self.rect.x - 3
+        #Get the right point so we can calculate when the image is fully off the screen. Then we need to set it back to its original position.
+        right = self.rect.x + (BACKGROUND_WIDTH * SCALE)
+        if right == 0:
+            self.rect.x = (BACKGROUND_WIDTH * SCALE)
 
 def main():
-    counter = 0;
+    counter = 0
     running = True
     print "Starting version", version
     pygame.init()
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     pygame.display.set_caption("Flappy Bird " + version)
-    screen = pygame.display.set_mode((SCREEN_WIDTH * SCALE, SCEREN_HEIGHT * SCALE))
+    screen = pygame.display.set_mode((SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE))
 
-    backgroundcrop = pygame.Surface((SCREEN_WIDTH, SCEREN_HEIGHT))
-    backgroundcrop.blit(spriteimage, (0, 0), (0, 0, SCREEN_WIDTH, SCEREN_HEIGHT))
-    backgroundcrop = pygame.transform.scale(backgroundcrop, (SCREEN_WIDTH * SCALE, SCEREN_HEIGHT * SCALE));
+    backgroundcrop = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+    backgroundcrop.blit(spriteimage, (0, 0), (0, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
+    backgroundcrop = pygame.transform.scale(backgroundcrop, (SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE))
 
-    pipe_list = pygame.sprite.Group();
-    background_list = pygame.sprite.Group();
+    pipe_list = pygame.sprite.Group()
+    background_list = pygame.sprite.Group()
 
-    scrollingBackground = ScrollingBackground()
-    background_list.add(scrollingBackground);
+    #Start one background at 0, start another right at the end of the other one.
+    #The X of the right point is BACKGROUND_WIDTH * SCALE
+    bg1 = ScrollingBackground(0)
+    bg2 = ScrollingBackground(BACKGROUND_WIDTH * SCALE)
+    background_list.add(bg1)
+    background_list.add(bg2)
 
     while running:
-        clock.tick(60);
+        clock.tick(60)
 
-        counter = counter + 1;
+        counter = counter + 1
 
         if counter == TICKS_TO_SPAWN:
-            counter = 0;
-            start = random.randint(5 * SCALE, 140 * SCALE) - (159 * SCALE);
+            counter = 0
+            #Generate a random number between 5 and 140.
+            #Adjust for SCALE
+            #Subtract the length of the pipe so so we get the "Top" pipe.
+            start = random.randint(5 * SCALE, 140 * SCALE) - (PIPE_HEIGHT * SCALE)
             topPipe = Pipe("top", start)
-            bottomPipe = Pipe("bottom", start + (159 * SCALE) + (50 * SCALE));
+            #Add at least 50 pixels of clearence for the player to navigate through.
+            bottomPipe = Pipe("bottom", start + (PIPE_HEIGHT * SCALE) + (50 * SCALE))
             pipe_list.add(topPipe)
             pipe_list.add(bottomPipe)
 
-        screen.fill((0, 0, 0))
         screen.blit(backgroundcrop, backgroundcrop.get_rect())
         pipe_list.draw(screen)
+
         background_list.draw(screen)
         pygame.display.flip()
 
-        background_list.update();
-        pipe_list.update();
-
+        background_list.update()
+        pipe_list.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
